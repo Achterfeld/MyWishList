1a610ad299268be18e5fe90240976fb654d5c989
Matéo Achterfeld <ma.achter@hotmail.fr>
2019-11-19 15:41:51 +0100

    ajout des fontions ville et liste dans les class Ville et Liste

--- src/modele/liste.php
+++ src/modele/liste.php
@@ -5,4 +5,8 @@ class Liste extends \Illuminate\Database\Eloquent\Model
     protected $table = 'liste';
     protected $primaryKey = 'no';
     public $timestamps = false;
+
+    public function item() {
+    	return $this->hasMany('\modele\Liste', 'liste_id');
+    }
 }
--- test.php
+++ test.php
@@ -1,8 +1,11 @@
 <?php
+
+require_once __DIR__ . '/vendor/autoload.php';
 use Illuminate\Database\Capsule\Manager as DB;
 
+
 $db = new DB();
-$db->addConnection($parse_ini_file("/src/conf/conf.ini"));
+$db->addConnection(parse_ini_file("src/conf/conf.ini"));
 
 $db->setAsGlobal();
 $db->bootEloquent();
