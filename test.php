f44c1cdbafd3474ade3155bcd0a9a81a0cdbe61e
Keller Guillaume <keller.guillaume68@gmail.com>
2019-11-19 15:52:10 +0100

    Affichage des listes

--- test.php
+++ test.php
@@ -3,9 +3,17 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 use Illuminate\Database\Capsule\Manager as DB;
+use wishlist\modele\Liste;
 
 $db = new DB();
 $db->addConnection(parse_ini_file("src/conf/conf.ini"));
 
 $db->setAsGlobal();
 $db->bootEloquent();
+
+
+$listes = Liste::get();
+
+foreach ($listes as $liste) {
+    echo $liste."<br>";
+}
