a88a6208e4465c81589e58f59086328c65ada007
Keller Guillaume <keller.guillaume68@gmail.com>
2019-11-22 14:35:06 +0100

    Ajout des méthodes de test basiques

--- test.php
+++ test.php
@@ -4,6 +4,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use Illuminate\Database\Capsule\Manager as DB;
 use wishlist\modele\Liste;
+use wishlist\modele\Item;
 
 
 $db = new DB();
@@ -12,9 +13,29 @@ $db->addConnection(parse_ini_file("src/conf/conf.ini"));
 $db->setAsGlobal();
 $db->bootEloquent();
 
+$args = $_GET;
+if (isset($args['val'])){
+    $it = Item::where('id','=',$args['val'])->get();
+    echo $it;
+}
+
+
+//echo $args['val'];
+
+
+
+echo "<br><br>";
 
 $listes = Liste::get();
 
 foreach ($listes as $liste) {
-    echo $liste."<br>";
+    echo $liste . "<br>";
+}
+echo "<br>";
+echo "<br>";
+
+$items = Item::get();
+
+foreach ($items as $item) {
+    echo $item . "<br>";
 }
