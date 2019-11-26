21473f12452e628178e8b2f8bccc23257a7402ed
Keller Guillaume <keller.guillaume68@gmail.com>
2019-11-26 15:49:22 +0100

    Ajout des méthodes du TD 12

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 use Illuminate\Database\Capsule\Manager as DB;
-use wishlist\modele\Liste;
-use wishlist\modele\Item;
+use wishlist\model\Liste;
+use wishlist\model\Item;
 
 
 $str = '<head>
@@ -12,7 +12,7 @@ $str = '<head>
 <meta name="robots" content="noindex,nofollow">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="icon" href="./img/favicon.ico" type="image/x-icon"><link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-<link rel="stylesheet" type="text/css" href="./style/index.css">
+<link rel="stylesheet" type="text/css" href="/MyWishList/style/style.css">
 <title>My Wish List</title>
 </head>';
 echo $str;
@@ -24,95 +24,47 @@ $db->addConnection(parse_ini_file("src/conf/conf.ini"));
 $db->setAsGlobal();
 $db->bootEloquent();
 
-$maxNo = Liste::max("no");
 
-$maxNoItem = Item::max("id");
+echo ('<div id="navBarre"> 
+<div>Bonjour, bienvenue dans MyWishList</div>
+<div style="flex:1"></div>
+<div><a href="./" >Se connecter</a></div>
+<div><a href="./" >S\'inscrire</a></div>
+</div>');
 
-echo(' 
-<div id = "bienvenue">Bonjour, bienvenue dans MyWishList</div>
-<div id = "boutonConnexion"><a href="./" >Se connecter</a></div>');
+$app = new \Slim\Slim;
 
-echo ('
+$app->get('/liste', function () {
+    echo "<h1>LISTE</h1>";
 
-<div id = "formulaires">
-<form action="test.php" method="get">
+    $listes = Liste::get();
 
-<h1>Affichage liste</h1>
-<input type="number" placeholder="Numéro de la liste à afficher" min="1" max="' . $maxNo . '" name="liste"><br>
-<input type="submit" value="Choisir sa liste"></input>
-
-</form>
-
-
-<form action="test.php" method="post">
-
-<h1>Création liste</h1>
-<input type="text" placeholder="Titre" name="titreNouvelleListe"><br>
-<input type="text" placeholder="Description" name="descriptionNouvelleListe"><br>
-<input type="text" placeholder="Titre" name="titreNouvelleListe"><br>
-Limite de validité : <input type="date" name="dateLimiteNouvelleListe"><br>
-<input type="submit" value="Ajouter une liste"></input>
-
-</form>
-
-
-<form action="test.php" method="post">
-
-<h1>Ajout dans liste</h1>
-<input type="number" placeholder="Liste dans laquelle ajouter" min="1" max="' . $maxNo . '" name="aAjouterDansListe"><br>
-<input type="number" placeholder="Objet à ajouter" min="1" max="' . $maxNoItem . '" name="itemAAjouter"><br>
-<input type="submit" value="Ajouter dans la liste"></input>
-
-</form>
-
-</div>
-');
-
-
-// Fonction de création d'un nouvel objet
-
-/*
-$i = new Item();
-$i->nom = "test";
-$i->liste_id = -1;
-$i->save();
-
-$i->liste_id = 1;
-$i->save();
-*/
-
-//METTRE CA DANS LE CONTROLER
-$args = $_GET;
-
-if (isset($args['liste'])) {
-    $listeDesc = Liste::where('no', '=', $args['liste'])->get()->first();
-    if (!is_null($listeDesc)) {
-        echo "<h1>Description de la liste " . $args['liste'] . " :</h1><br> " . $listeDesc;
+    foreach ($listes as $liste) {
+        echo $liste . "<br><br>";
     }
-}
+});
+
+$app->get('/item/:id', function ($id) {
+    echo "<h1>Item n° $id</h1>";
 
 
-if (isset($args['id'])) {
-    $it = Item::where('id', '=', $args['id'])->get()->first();
-    echo "<h1>L'item d'id " . $args['id'] . " est le suivant : </h1><br> " . $it;
-}
+    $it = Item::where('id', '=', $id)->get()->first();
+    echo "<h1>L'item d'id " . $id . " est le suivant : </h1><br> " . $it;
+});
 
-//echo $args['val'];
+$app->get('/liste/:id', function ($id) {
+    echo "<h1>liste n° $id</h1>";
 
-echo "<br><br>";
-echo "<h1>Liste des listes créées :</h1> <br> ";
 
-$listes = Liste::get();
 
-foreach ($listes as $liste) {
-    echo $liste . "<br><br>";
-}
-echo "<br>";
-echo "<br>";
-echo "<h1>Liste des items créés :</h1> <br> ";
+    $listeDesc = Liste::where('no', '=', $id)->get()->first();
+    if (!is_null($listeDesc)) {
+        echo "<h1>Description de la liste " . $id . " :</h1><br> " . $listeDesc;
+    }
+});
 
-$items = Item::get();
+$app->get('/', function () {
+    echo "<h1>index</h1>";
+});
 
-foreach ($items as $item) {
-    echo $item . "<br><br>";
-}
+$app->run();
