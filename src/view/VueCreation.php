9bbaebe0ac1073cd57b0b780826d89da419d6ce9
Keller Guillaume <keller.guillaume68@gmail.com>
2019-12-04 15:59:56 +0100

    Vue pour la création (liste + item)

---
+++ src/view/VueCreation.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace wishlist\view;
+
+class VueCreation
+{
+    const LIST = 1;
+    const ITEM = 2;
+
+    function afficheCreationListe()
+    {
+
+        $html = <<<END
+        <form action="test.php" method="post">
+
+            <h1>Création liste</h1>
+            <input type="text" placeholder="Titre" name="titreNouvelleListe"><br>
+            <input type="text" placeholder="Description" name="descriptionNouvelleListe"><br>
+            Limite de validité : <input type="date" name="dateLimiteNouvelleListe"><br>
+            <input type="submit" value="Ajouter une liste"></input>
+
+        </form>
+
+END;
+        return $html;
+    }
+
+    function afficheCreationItem()
+    {
+
+        $html = <<<END
+        <form action="test.php" method="post">
+
+            <h1>Création liste</h1>
+            <input type="text" placeholder="Titre" name="titreNouvelleListe"><br>
+            <input type="text" placeholder="Description" name="descriptionNouvelleListe"><br>
+            <input type="text" placeholder="Titre" name="titreNouvelleListe"><br> Limite de validité : <input type="date" name="dateLimiteNouvelleListe"><br>
+            <input type="submit" value="Ajouter une liste"></input>
+
+        </form>
+
+END;
+        return $html;
+    }
+
+
+
+    function render($selecter)
+    {
+        switch ($selecter) {
+
+            case self::LIST:
+                $content = $this->afficheCreationListe();
+                break;
+            case self::ITEM:
+                $content = $this->afficheCreationItem();
+                break;
+        }
+
+        VueGenerale::renderPage($content);
+    }
+}
