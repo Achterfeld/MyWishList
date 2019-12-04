c5ab7a689f86d72a8ab1e7e58f1442d4345c5394
Matéo Achterfeld <ma.achter@hotmail.fr>
2019-12-04 15:37:54 +0100

    correction maj dans le header

--- src/view/VueGenerale.php
+++ src/view/VueGenerale.php
@@ -14,7 +14,7 @@ class VueGenerale
         <meta name="robots" content="noindex,nofollow">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="icon" href="./img/favicon.ico" type="image/x-icon"><link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-        <link rel="stylesheet" type="text/css" href="/MyWishList/style/style.css">
+        <link rel="stylesheet" type="text/css" href="/myWishList/style/style.css">
         <title>My Wish List</title>
         </head>
 END;
