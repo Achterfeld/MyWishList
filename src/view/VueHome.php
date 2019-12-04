0978643d1ad1d7608d831d3964ab1a04b1eb4b34
Keller Guillaume <keller.guillaume68@gmail.com>
2019-12-04 15:25:10 +0100

    MAJ Graphique Home

--- src/view/VueHome.php
+++ src/view/VueHome.php
@@ -6,7 +6,40 @@ class VueHome
 {
     public function render()
     {
+        $header = VueGenerale::renderHeader();
         $html=<<<END
+        $header
+
+        <div id="topBarre">
+        <img style="height:20px;width:20px" href="">
+        <a class="boutton small" href="">S'inscrire</a>
+        <a class="boutton small" href="">Se connecter</a>
+    </div>
+
+
+    <H1 class="titre">My Wish List</H1>
+
+
+    <div class="deuxColonnes">
+        <div>
+            <form>
+                <h1>S'inscrire</h1>
+                <input type="text" placeholder="Nom" name="Nom"><br>
+                <input type="text" placeholder="Prenom" name="Prenom"><br>
+                <input type="date" placeholder="Date de naissance" name="DNaiss"><br>
+                <input type="email" placeholder="Mail" name="Mail"><br>
+                <input class="boutton" type="submit" value="S'inscrire"></input>
+
+                <a href="./">Déjà inscrit ?</a>
+            </form>
+
+        </div>
+        <div>
+            <h1>Pas besoin de compte ?</h1>
+            <br>
+            <a href="./" class="boutton">Mode invité</a>
+        </div>
+    </div>
         <div>
     <a href="./liste" > La liste de toutes les listes </a>
 </div>
@@ -18,7 +51,7 @@ class VueHome
 <br>
 END;
 
-        VueGenerale::renderPage($html);
+echo $html;
 
     }
 }
