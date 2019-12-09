21dcefcd991a4b4090d872f60363004bf7c239c2
totomdotcom <tom.georgelin@gmail.com>
2019-12-09 13:45:21 +0100

    Modification méthode post pour reservation

--- index.php
+++ index.php
@@ -98,8 +98,9 @@ $app->post('/inscription', function () {
 });
 
 // permet de réserver un item dans une liste
-$app->post('/reserver', function() {
-
+$app->post('/reserver/:id/:participant', function($id, $participant) {
+    $c = new ListeControler();
+    $c->reserverItemListe($id, $participant);
 });
 
 $app->run();
