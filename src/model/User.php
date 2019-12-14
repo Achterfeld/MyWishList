5cb3c89dee580a049af719472d9bf20393a22ea1
Keller Guillaume <keller.guillaume68@gmail.com>
2019-12-14 22:56:35 +0100

    Ajout association

--- src/model/User.php
+++ src/model/User.php
@@ -7,4 +7,9 @@ class User extends \Illuminate\Database\Eloquent\Model
     protected $table = 'user';
     protected $primaryKey = 'user_id';
     public $timestamps = false;
+
+    public function listes()
+    {
+        return $this->hasMany('wishlist\model\Liste', 'user_id');
+    }
 }
