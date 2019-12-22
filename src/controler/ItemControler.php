3b2f551d9f3201a43a20f511c95aa19096ec229f
Keller Guillaume <keller.guillaume68@gmail.com>
2019-12-22 20:17:19 +0100

    Create ItemControler.php
    Ajout d'un controleur pour l'ajout d'item

---
+++ src/controler/ItemControler.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace wishlist\controler;
+
+use \Illuminate\Database\Capsule\Manager as DB;
+use wishlist\model\Liste;
+use wishlist\model\Item;
+use wishlist\view\VueParticipant;
+use wishlist\view\VueCreation;
+use wishlist\view\VueModification;
+
+class ItemControler
+{
+
+
+    public function getCreation($no, $token)
+    {
+        $v = new VueCreation();
+        $v->render(VueCreation::ITEM, $token, $no);
+    }
+
+
+    public function validerItem()
+    {
+        if (isset($_SESSION['session'])) {
+
+            $app = new \Slim\Slim;
+            $i = new Item();
+            $datas = $app->request();
+
+            $i->liste_id = $datas->post('noListe');
+            $i->nom = filter_var($datas->post('nomItem'), FILTER_SANITIZE_SPECIAL_CHARS);
+            $i->descr = filter_var($datas->post('descriptionItem'), FILTER_SANITIZE_SPECIAL_CHARS);
+
+            $url = filter_var($datas->post('URL'), FILTER_SANITIZE_SPECIAL_CHARS);
+            $i->url = $url;
+
+            $i->tarif = $datas->post('prixItem');
+            $i->save();
+
+            $this->getItem($i->id);
+        } else {
+            echo ("Vous devez être connecté pour modifier/ajouter un article dans une liste");
+        }
+    }
+    public function getItem($id)
+    {
+        $i = Item::where('id', '=', $id)->first();
+        $v = new VueParticipant($i);
+        $v->render(VueParticipant::ITEM_VIEW);
+    }
+}
