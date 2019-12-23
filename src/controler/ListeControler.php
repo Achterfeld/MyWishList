b6d8b89ccb86ceac48ea4236e780a54403a4782c
Keller Guillaume <keller.guillaume68@gmail.com>
2019-12-23 23:15:39 +0100

    Ajout formulaire + affichage
    Ajout du formulaire pour ajouter un message sur une liste
    + afficher tous les messages de la liste

--- src/controler/ListeControler.php
+++ src/controler/ListeControler.php
@@ -24,14 +24,16 @@ class ListeControler
         $v->render(VueParticipant::ITEM_VIEW);
     }
 
-    public function getItemListe($idItem) {
-        $i = Item::where('id', '=',$idItem)->first();
+    public function getItemListe($idItem)
+    {
+        $i = Item::where('id', '=', $idItem)->first();
         $v = new VueParticipant($i);
         $v->afficheItemListe($i);
     }
 
-    public function getAllListe() {
-        $liste = Liste::where('public','=','1')->OrderBy('expiration')->get();
+    public function getAllListe()
+    {
+        $liste = Liste::where('public', '=', '1')->OrderBy('expiration')->get();
         $v = new VueParticipant($liste);
         $v->render(VueParticipant::ALL_LIST_VIEW);
     }
@@ -45,7 +47,7 @@ class ListeControler
 
     public function getListe($no, $token)
     {
-        $l = Liste::where([['no', '=', $no],['token', '=', $token]])->first();
+        $l = Liste::where([['no', '=', $no], ['token', '=', $token]])->first();
         $v = new VueParticipant($l);
         $v->render(VueParticipant::LIST_VIEW);
     }
@@ -74,74 +76,75 @@ class ListeControler
         if (isset($_SESSION['session'])) {
             $l->user_id = $_SESSION['session']['user_id'];
         } else {
-            $l->user_id = -1;   
+            $l->user_id = -1;
         }
 
-        $l->expiration = filter_var($datas->post("dateLimiteNouvelleListe"),FILTER_SANITIZE_SPECIAL_CHARS);
-        $l->titre = substr(filter_var($datas->post("titreNouvelleListe"),FILTER_SANITIZE_SPECIAL_CHARS),0,256);
-        $l->description = filter_var($datas->post("descriptionNouvelleListe"),FILTER_SANITIZE_SPECIAL_CHARS);
+        $l->expiration = filter_var($datas->post("dateLimiteNouvelleListe"), FILTER_SANITIZE_SPECIAL_CHARS);
+        $l->titre = substr(filter_var($datas->post("titreNouvelleListe"), FILTER_SANITIZE_SPECIAL_CHARS), 0, 256);
+        $l->description = filter_var($datas->post("descriptionNouvelleListe"), FILTER_SANITIZE_SPECIAL_CHARS);
         $l->token = $token;
 
         $l->save();
     }
 
-    public function addRes($id) {
+    public function addRes($id)
+    {
         $app = new \Slim\Slim;
         $i = Item::where('id', '=', $id)->first();
 
-        $i->reservation = filter_var($app->request()->post("participant"),FILTER_SANITIZE_SPECIAL_CHARS);
-        $i->reservation = substr(filter_var($app->request()->post("message"),FILTER_SANITIZE_SPECIAL_CHARS),0,256);
+        $i->reservation = filter_var($app->request()->post("participant"), FILTER_SANITIZE_SPECIAL_CHARS);
+        $i->reservation = substr(filter_var($app->request()->post("message"), FILTER_SANITIZE_SPECIAL_CHARS), 0, 256);
 
         $i->save();
     }
 
 
-    public function modifierListe($no,$token){
+    public function modifierListe($no, $token)
+    {
 
-        $app = new \Slim\Slim;
-        $l = Liste::where([['no', '=', $no],['token','=',$token]])->first();
+        //        $app = new \Slim\Slim;
+        $l = Liste::where([['no', '=', $no], ['token', '=', $token]])->first();
 
         $v = new VueModification();
-        $v->render(VueModification::LIST,$l);
-
+        $v->render(VueModification::LIST, $l);
     }
 
-	public function validerListe($no,$token){
-        
+    public function validerListe($no, $token)
+    {
+
         $app = new \Slim\Slim;
-        $l = Liste::where([['no', '=', $no],['token','=',$token]])->first();
+        $l = Liste::where([['no', '=', $no], ['token', '=', $token]])->first();
 
         $datas = $app->request();
 
-        $l->expiration = filter_var($datas->post("dateLimiteNouvelleListe"),FILTER_SANITIZE_SPECIAL_CHARS);
-        $l->titre = substr(filter_var($datas->post("titreNouvelleListe"),FILTER_SANITIZE_SPECIAL_CHARS),0,256);
-        $l->description = filter_var($datas->post("descriptionNouvelleListe"),FILTER_SANITIZE_SPECIAL_CHARS);
+        $l->expiration = filter_var($datas->post("dateLimiteNouvelleListe"), FILTER_SANITIZE_SPECIAL_CHARS);
+        $l->titre = substr(filter_var($datas->post("titreNouvelleListe"), FILTER_SANITIZE_SPECIAL_CHARS), 0, 256);
+        $l->description = filter_var($datas->post("descriptionNouvelleListe"), FILTER_SANITIZE_SPECIAL_CHARS);
         $l->public = $datas->post("visib");
         $l->save();
-
     }
 
-    public function confirmerSupprListe($no,$token){
+    public function confirmerSupprListe($no, $token)
+    {
         $v = new VueCreation();
-        $v->render(VueCreation::SUPPR_LIST,$token,$no);
+        $v->render(VueCreation::SUPPR_LIST, $token, $no);
     }
 
-    public function supprimer($no,$token){
-        $l = Liste::where([['no','=',$no],['token','=',$token]]);
+    public function supprimer($no, $token)
+    {
+        $l = Liste::where([['no', '=', $no], ['token', '=', $token]]);
         $l->delete();
 
 
         if (isset($_SESSION['session']['user_id'])) {
 
             $v = new VuePagePerso();
-            $u = User::where('user_id','=',$_SESSION['session']['user_id'])->first();
-            $v->render($u,"Liste supprimée");
-        }else{
+            $u = User::where('user_id', '=', $_SESSION['session']['user_id'])->first();
+            $v->render($u, "Liste supprimée");
+        } else {
             $app = new \Slim\Slim;
-            $app->redirect("/myWishList");
-            
+
+            $app->response->redirect("/myWishList", 303);
         }
-
     }
-
 }
--- src/model/Liste.php
+++ src/model/Liste.php
@@ -13,19 +13,25 @@ class Liste extends \Illuminate\Database\Eloquent\Model
         return $this->hasMany('wishlist\model\Item', 'liste_id');
     }
 
-    public function possede(){
-        return $this->belongsTo('wishlist\model\User','user_id');
+    public function messages()
+    {
+        return $this->hasMany('wishlist\model\MessagesListes', 'liste_id');
+    }
+
+    public function possede()
+    {
+        return $this->belongsTo('wishlist\model\User', 'user_id');
     }
 
 
     public function __toString()
     {
 
-        $public = $this->public?"<span class='public'>🌎 publique":"<span class='priv'>🔒 privée";
+        $public = $this->public ? "<span class='public'>🌎 publique" : "<span class='priv'>🔒 privée";
 
-        $possede=$this->possede()->first()->prenom;
+        $possede = $this->possede()->first()->prenom;
 
-        $str=<<<END
+        $str = <<<END
 
         <div class='list'>
         <div class='num num_liste'>$this->no</div>
@@ -35,7 +41,7 @@ class Liste extends \Illuminate\Database\Eloquent\Model
         <div>Visibilité : $public</div></span><br><br>
 
 END;
-/*|token $this->token " ;*/
+        /*|token $this->token " ;*/
 
         $itDedans = $this->item()->get();
         $str .= "<ul>";
@@ -46,6 +52,39 @@ END;
             $str .= "<li><a class='lienSCouleur' href='/myWishList/item/reservation/$value->id'>$value->nom $reserv</a></li> ";
         }
         $str .= "</ul></div>";
+
+
+
+
+
+        $auteur = isset($_SESSION['session']) ? $_SESSION['session']['prenom'] : "''";
+
+
+        $str .= <<<END
+
+        <div style="margin: 1em;">
+            <form class="formulaire" method="post" action="/myWishList/ajout/message/$this->no/$this->token">
+                <div>Ajouter un message :</div>
+                <input type="text" name="auteur" placeholder="Votre nom" value=$auteur><br>
+                <input type="text" name="message" placeholder="Votre message pour l'organisateur" ><br>
+                <input type="submit" value="Ajoutez votre message" ></input><br>
+            </form>
+        </div>
+
+
+END;
+
+        $mess = $this->messages()->get();
+
+        foreach ($mess as $key => $value) {
+            $str .= <<<END
+            <div class="messageListe">
+                <p><h1>$value->auteur : </h1><br>“ $value->message ”</p>
+            </div>
+END;
+        }
+
+
 
         return $str;
     }
