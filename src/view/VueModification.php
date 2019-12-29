3116cb3d374efd97f631711cbf8034530299424d
Keller Guillaume <keller.guillaume68@gmail.com>
2019-12-29 19:16:28 +0100

    Nouvelle page modification d'item
    Ajout de la page de modification des items, basé sur le même format que la modification de liste

--- src/view/VueModification.php
+++ src/view/VueModification.php
@@ -2,6 +2,8 @@
 
 namespace wishlist\view;
 
+use wishlist\model\Liste;
+
 class VueModification
 {
     const LIST = 1;
@@ -26,8 +28,8 @@ class VueModification
         <form action="$token" method="post" class="formulaire">
 
             <h1>Modification liste</h1>
-            <input type="text" value="$titre" placeholder="Titre" name="titreNouvelleListe"><br>
-            <input type="text" value="$desc" placeholder="Description" name="descriptionNouvelleListe"><br>
+            <input type="text" value="$titre" placeholder="Titre" name="titreNouvelleListe" maxlength="256"><br>
+            <input type="text" value="$desc" placeholder="Description" name="descriptionNouvelleListe" maxlength="256"><br>
             Limite de validité : <input type="date" value="$exp" name="dateLimiteNouvelleListe"><br>
             
             $visibilite
@@ -77,21 +79,79 @@ END;
         return $txt;
     }
 
+
     function afficheModificationItem($modif)
     {
+        $reservation = $modif->reservation;
+        $id = $modif->id;
 
-        $html = <<<END
-        <form action="./" method="post" class="formulaire">
+        $html = "";
 
-            <h1>Création liste</h1>
-            <input type="text" placeholder="Titre" name="titreNouvelleListe"><br>
-            <input type="text" placeholder="Description" name="descriptionNouvelleListe"><br>
-            <input type="text" placeholder="Titre" name="titreNouvelleListe"><br> Limite de validité : <input type="date" name="dateLimiteNouvelleListe"><br>
-            <input type="submit" value="Ajouter une liste"></input>
+        if (isset($_SESSION['session'])) {
+
+            $liste = $modif->liste()->first();
+
+            $userID = -1;
+
+            if (!is_null($liste)) {
+                $user = $liste->possede()->first();
+                if (!is_null($user)) {
+                    $userID = $user->user_id;
+                }
+            }
+
+            if (is_null($reservation) && $userID == $_SESSION['session']['user_id']) {
+
+                $nom = $modif->nom;
+                $descr = $modif->descr;
+                $img = $modif->img;
+                $url = $modif->url;
+                $tarif = $modif->tarif;
+
+                $app = \Slim\Slim::getInstance();
+                //$urlItemPresuppression = $app->urlFor('route_presuppressionItem', ['id' => $id]);
+                $urlItemPresuppression = ""; //$app->urlFor('route_presuppressionItem', ['id' => $id]);
+
+
+                //#############################
+
+                $app = \Slim\slim::getInstance();
+                $urlpostValidItem = $app->urlFor('route_post_validationItem');
+
+                $html = <<<END
+        <form action="$id" method="post" class="formulaire">
+
+            <h1>Modification item</h1>
+            <input type="text" name="nomItem" value='$nom' placeholder="Nom" required><br>
+            <input type="text" name="descriptionItem" value='$descr' placeholder="Description" maxlength="256" required><br>
+            <input type="text" name="URLImage" value='$img' placeholder="Url de l'image"><br>
+            <input type="number" name="prixItem" value='$tarif' min="0.01" max="99999.99" step="any" placeholder="Prix" required><br>
+            <input type="url" name="URL" value='$url' placeholder="Lien vers une page de description ? (optionnel)"><br>
+
+            <input type="submit" value="Modifier l'item"></input>
 
         </form>
 
+        <br>
+
+        <form class='formulaire redBG' action="$urlItemPresuppression" method="post">
+            <input type="submit" value="❌Supprimer l'item">
+        </form>
+
+
 END;
+                //#############################
+
+            } else {
+                $app = \Slim\Slim::getInstance();
+                $urlItem = $app->urlFor('route_itemID', ['id' => $id]);
+                $app->response->redirect($urlItem, 303);
+            }
+        } else {
+            $app = \Slim\Slim::getInstance();
+            $urlItem = $app->urlFor('route_itemID', ['id' => $id]);
+            $app->response->redirect($urlItem, 303);
+        }
         return $html;
     }
 
